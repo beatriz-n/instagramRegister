10f9f5c810db2a6ae3c051dd168c073374963264
beatriz <beatriznespolid@gmail.com>
2022-08-10 19:46:42 -0300

    vuetify

--- prod/src/components/camposComp.vue
+++ prod/src/components/camposComp.vue
@@ -2,9 +2,13 @@
 <div class="form">
 <h1>Sistema</h1>
 <h5>Cadastre-se para ver fotos e vídeos dos seus amigos.</h5>
+<button>Entrar com o Facebook</button>
+<p>ou</p>
     <input type="text" value="text" placeholder="Número de celular ou email">
     <input type="text" value="text" placeholder="Nome completo">
     <input type="text" value="text" placeholder="Senha">
+    <p>Ao se cadastra, você concorda com nossos <b>Termos, Politicas de Privacidade e Politica de Cookies</b></p>
+    <button>Cadastre-se</button>
 </div>
 </template>
 <script>
@@ -23,6 +27,14 @@ h5{
 }
 .form {
     text-align: center;
+}
+p, b{
+   color: rgb(163, 163, 163);
+font-size: 10px;
+}
+button{
+     background-color: rgb(36, 147, 244);
+     color: aliceblue;
 }
 
 </style>
